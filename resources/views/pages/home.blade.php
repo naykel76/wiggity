fddeae995d79342345959350bce6ccac66df3346
Nathan Watts <nathan@naykel.com.au>
2025-10-02 11:41:19 +1000

    install gotime

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,4 @@
+<x-gt-app-layout layout="{{ config('gotime.template') }}" hasContainer class="py-5-3-2-2">
+
+
+</x-gt-app-layout>
--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,45 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig, loadEnv } from 'vite';
+import laravel from "laravel-vite-plugin";
+import purge from "@erbelion/vite-plugin-laravel-purgecss";
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), '')
+    const isDev = mode === 'development';
+    const isStaging = mode === 'staging';
+    const VITE_APP_URL = env.VITE_APP_URL;
+
+    return {
+        ...(isDev && {
+            server: {
+                open: VITE_APP_URL,
+                watch: {
+                    ignored: ['**/node_modules/**', '**/storage/**', '**/vendor/**', '!**/vendor/naykel/**'],
+                },
+                port: 5187
+            },
         }),
-        tailwindcss(),
-    ],
+        plugins: [
+            laravel({
+                input: ['resources/scss/app.scss', 'resources/js/app.js'],
+                refresh: true
+            }),
+            // purge({
+            //     paths: [
+            //         'resources/views/**/*.blade.php',
+            //         'vendor/naykel/**/*.php'
+            //     ],
+            //     safelist: {
+            //         standard: [/^\:has$/, /^\:is$/, /^\:not$/, /^\:where$/, /^\:hover$/, /^\:disabled$/, /^\:focus$/, /^\:active$/],
+            //     },
+            //     extractors: [
+            //         {
+            //             extractor: (content) => {
+            //                 return content.match(/[A-Za-z0-9-_:\/]+/g) || []
+            //             },
+            //             extensions: ['css', 'html', 'vue', 'php']
+            //         },
+            //     ],
+            // })
+        ],
+    }
 });
